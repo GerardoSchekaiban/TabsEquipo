0600f38aa77c3290229c3698fe9069d6b1ab1310
Gerardo <ggschekaiban@hotmail.com>
2025-10-31 17:54:06 -0600

    Initial Commit

---
+++ TabsTests/TabsTests.swift
@@ -0,0 +1,17 @@
+//
+//  TabsTests.swift
+//  TabsTests
+//
+//  Created by Gerardo Gomez Schekaiban on 31/10/25.
+//
+
+import Testing
+@testable import Tabs
+
+struct TabsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
