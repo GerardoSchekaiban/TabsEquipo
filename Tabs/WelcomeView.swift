e96d04247558a92e65bf250b1cacbd744d22e60d
victor <victor.deleon@iest.edu.mx>
2025-11-03 18:18:07 -0600

    New window view lol

---
+++ Tabs/WelcomeView.swift
@@ -0,0 +1,45 @@
+//
+//  WelcomeView.swift
+//  Tabs
+//
+//  Created by win603 on 03/11/25.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    @AppStorage("myName") var savedName: String = ""
+    @AppStorage("myAge") var savedAge: Int = 0
+    @State var name: String = ""
+
+    @State var ageText: String = ""
+    
+    var body: some View {
+        VStack{
+            TextField("Dime tu nombre:", text: $name)
+                .padding()
+            
+            TextField("Dime tu edad:", text: $ageText)
+                .padding()
+                
+            Button(action: {
+                savedName = self.name
+                if let edad = Int (self.ageText){
+                  savedAge = edad
+                }
+            }, label: {
+                Text("Guardar").padding().background(Color.green)
+            }).padding()
+            
+            
+            Text("Bienvenido \(savedName) y edad \(savedAge)")
+        }
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
+
